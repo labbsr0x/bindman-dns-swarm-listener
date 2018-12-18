4861e0828e7150bfb045f7b45dde457ca6c8fc50
Abilio Esteves <eco.abilio@gmail.com>
2018-12-18 11:22:33 -0200

    added basic sync

--- src/listener/misc.go
+++ src/listener/misc.go
@@ -34,8 +34,8 @@ type SandmanService struct {
 }
 
 // backoffWait sleeps thread exponentially longer depending on the trial index
-func backoffWait(max uint, triesLeft uint) {
-	waitSeconds := time.Duration(math.Exp2(float64(max-triesLeft))+1) * time.Second
+func backoffWait(max uint, triesLeft uint, baseDuration time.Duration) {
+	waitSeconds := time.Duration(math.Exp2(float64(max-triesLeft))+1) * baseDuration
 	time.Sleep(waitSeconds)
 }
 
